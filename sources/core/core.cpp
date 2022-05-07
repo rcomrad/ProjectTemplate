312ec6b4cf60b0dd1a3f88a215d4caafaeed184f
Александр Федоров <fedorovas@int.adtspb.ru>
2022-05-07 16:39:25 +0300

    start adding sfml

--- sources/core/core.cpp
+++ sources/core/core.cpp
@@ -1,7 +1,30 @@
 #include "core/core.hpp"
+#include <SFML/Graphics.hpp>
 
 proj::Core::Core()
 {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return;
+
+
+
     std::string path = OUTPUT_PATH + "test.test";
 
     dom::File mFile(OUTPUT_PATH + "test.test");
